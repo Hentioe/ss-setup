ca69a13e53a77d2695c816ab69a9f57a13430d71
Hentioe <meow.i5.br@gmail.com>
2024-05-18 19:05:20 +0800

    Improve update whitelist script

--- update_whitelist.sh
+++ update_whitelist.sh
@@ -2,14 +2,16 @@
 
 # Add whitelist IPv4 to ipset
 for ip in $(cat 'whitelist.txt'); do
-    if ! ipset test chnroute $ip; then
+    if ! ipset test chnroute $ip >/dev/null 2>&1; then
         ipset add chnroute $ip
+        echo "Add $ip to chnroute"
     fi
 done
 
 # Add whitelist IPv6 to ipset
 for ip in $(cat 'whitelist6.txt'); do
-    if ! ipset test chnroute6 $ip; then
+    if ! ipset test chnroute6 $ip >/dev/null 2>&1; then
         ipset add chnroute6 $ip
+        echo "Add $ip to chnroute6"
     fi
 done
