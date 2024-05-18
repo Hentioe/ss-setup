1aaea672546c07d04f594201b9eb8509e80fbffa
Hentioe <meow.i5.br@gmail.com>
2024-05-18 19:19:18 +0800

    Fix update ip lists script

--- update_ip_lists.sh
+++ update_ip_lists.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# Download the file
-curl http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -o "delegated-apnic-latest.txt"
+local_file="delegated-apnic-latest.txt"
+
+# Download the delegated-apnic-latest file
+curl http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -o "$local_file"
 
 # Generate china-ipv4.txt from the local file
 awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}' "$local_file" >china-ipv4.txt
 
+echo -e "\nchina-ipv4.txt has been generated"
+
 # Generate china-ipv6.txt from the local file
 awk -F '|' '/CN/&&/ipv6/ {print $4 "/" $5}' "$local_file" >china-ipv6.txt
+
+echo "china-ipv6.txt has been generated"
 
 # Clean up
 rm "$local_file"
