a92ff62eb988c91c475041e6e42207c09955ec64
Hentioe <meow.i5.br@gmail.com>
2024-06-02 09:16:58 +0800

    Add china-ip-full.txt generation

--- update_ip_lists.sh
+++ update_ip_lists.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 
-local_file="delegated-apnic-latest.txt"
+local_file=".delegated-apnic-latest.txt"
 
 # Download the delegated-apnic-latest file
 curl http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -o "$local_file"
@@ -14,6 +14,10 @@ echo -e "\nchina-ipv4.txt has been generated"
 awk -F '|' '/CN/&&/ipv6/ {print $4 "/" $5}' "$local_file" >china-ipv6.txt
 
 echo "china-ipv6.txt has been generated"
+
+cat china-ipv4.txt china-ipv6.txt >china-ip-full.txt
+
+echo "china-ip-full.txt has been generated"
 
 # Clean up
 rm "$local_file"
