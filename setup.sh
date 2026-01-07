8806d386abc3321f29aa98f7e06778d38c20e0cf
Hentioe <meow.i5.br@gmail.com>
2026-01-08 04:42:01 +0800

    Simplify direct/bypass IP list

--- setup.sh
+++ setup.sh
@@ -37,9 +37,9 @@ import_to_set "chnroute" "dist/china-ipv4.txt whitelist/4.txt"
 # 4. Add China routing table and whitelist for IPv6
 import_to_set "chnroute6" "dist/china-ipv6.txt whitelist/6.txt"
 
-# 5. Bypass LANs and reserved addresses (Optimized to single lines)
-nft add rule inet ss_nat SHADOWSOCKS ip daddr { 0.0.0.0/8, 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.168.0.0/16, 224.0.0.0/4, 240.0.0.0/4 } return
-nft add rule inet ss_nat SHADOWSOCKS ip6 daddr { ::1/128, fc00::/7, fe80::/10, fd00::/8 } return
+# 5. Bypass local/private/link-local/loopback/multicast traffic
+nft add rule inet ss_nat SHADOWSOCKS ip daddr { 127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16, 169.254.0.0/16, 224.0.0.0/4 } return
+nft add rule inet ss_nat SHADOWSOCKS ip6 daddr { ::1/128, fe80::/10, fc00::/7, ff00::/8 } return
 
 # 6. Bypass China IP sets
 nft add rule inet ss_nat SHADOWSOCKS ip daddr @chnroute return
